e16e99407d90c3a690e160bd85563e9210798ec8
Jeff Jia <jeff.jia@we-are-mea.com>
2026-01-23 16:27:46 +1300

    implement (buggy) winning check

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ export default function Board() {
 
     function handleClick(i) {
         const arrayCopy = squares.slice()
+
+        // if piece exist on i, skip
         if (squares[i]) {
             return
         }
@@ -17,6 +19,10 @@ export default function Board() {
         }
 
         setSquares(arrayCopy)
+        if (calculateWinner(arrayCopy)) {
+            window.alert('game over')
+        }
+
         setXIsNext(!xIsNext)
     }
 
@@ -47,4 +53,24 @@ function Square({ value, onSquareClick }) {
             {value}
         </button>
     )
+}
+
+function calculateWinner(squares) {
+    const lines = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+    ]
+    for (let i = 0; i < lines.length; i++) {
+        const [a, b, c] = lines[i]
+        if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+            return squares[a]
+        }
+    }
+    return null
 }
